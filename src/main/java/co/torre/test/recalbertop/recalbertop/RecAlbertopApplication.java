c18110e726637c85f6ded03b5ef7fde668c9264d
Alberto de Paola <alberto.paola@cwi.com.br>
2019-02-21 21:53:49 -0300

    Initial commit from spring initializr

---
+++ src/main/java/co/torre/test/recalbertop/recalbertop/RecAlbertopApplication.java
@@ -0,0 +1,13 @@
+package co.torre.test.recalbertop.recalbertop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecAlbertopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecAlbertopApplication.class, args);
+	}
+
+}
