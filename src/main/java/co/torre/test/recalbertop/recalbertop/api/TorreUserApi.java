b5d712b0fc7e9239c21dd1a9c209acd8cfd33aad
Alberto de Paola <alberto.paola@cwi.com.br>
2019-02-21 23:09:16 -0300

    adding cors config to endpoint

--- src/main/java/co/torre/test/recalbertop/recalbertop/api/TorreUserApi.java
+++ src/main/java/co/torre/test/recalbertop/recalbertop/api/TorreUserApi.java
@@ -1,6 +1,7 @@
 package co.torre.test.recalbertop.recalbertop.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import co.torre.test.recalbertop.recalbertop.dto.TorreUserDto;
 import co.torre.test.recalbertop.recalbertop.facade.TorreApiFacade;
 
+@CrossOrigin(origins = {"http://example.com", "http://localhost:4200", "http://localhost", "https://albertodepaola.github.io"}, maxAge = 3600)
 @RestController
 public class TorreUserApi {
 
